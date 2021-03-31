6faaccbad6e7f5cc78dc43aefec4d06d3ed6baad
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-03-31 10:18:23 +0100

    removes lru cache from pg statements

--- app/database/postgres.py
+++ app/database/postgres.py
@@ -80,7 +80,7 @@ class Connection:
 
     def __init__(self, conn_str=CONN_STR):
         self.conn_str = conn_str
-        self._connection = connect(self.conn_str)
+        self._connection = connect(self.conn_str, statement_cache_size=0)
 
     def __await__(self):
         yield from self._connection.__await__()
