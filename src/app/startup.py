1d998d9f607e2b170606d30f577b9b70c141f905
Jamie Hopper <jamie.hopper@kainos.com>
2022-03-09 13:12:24 +0000

    refac: swagger doc endpoint
    include swagger endpoint on all instances

--- src/app/startup.py
+++ src/app/startup.py
@@ -71,7 +71,6 @@ def start_app():
     app = FastAPI(
         title="UK Coronavirus Dashboard - API Service",
         version="2.1.0",
-        docs_url=None,
         redoc_url=None,
         openapi_url="/api/v2/openapi.json",
         middleware=middlewares,
